2c570683571578557a955c65c6fba7684a043415
garon <21.gubacsi.aron.mate@mechwart.com>
2026-04-11 19:37:46 +0200

    cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ android {
 }
 
 dependencies {
+    // my imports
+    implementation(libs.compose.google.fonts)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
