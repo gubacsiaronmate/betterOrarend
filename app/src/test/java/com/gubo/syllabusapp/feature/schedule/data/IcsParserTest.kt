580f0803284d7c66edb955776e4e0681198bfd7d
garon <21.gubacsi.aron.mate@mechwart.com>
2026-04-15 11:15:08 +0200

    parser done

--- app/src/test/java/com/gubo/syllabusapp/feature/schedule/data/IcsParserTest.kt
+++ app/src/test/java/com/gubo/syllabusapp/feature/schedule/data/IcsParserTest.kt
@@ -76,4 +76,40 @@ class IcsParserTest {
         assertEquals(10, result[0].endTime.withZoneSameInstant(zone).hour)
         assertEquals(40, result[0].endTime.withZoneSameInstant(zone).minute)
     }
+
+    @Test
+    fun `parse returns all sessions from a multi event ICS`() {
+        val icsContent = """
+            BEGIN:VCALENDAR
+            BEGIN:VEVENT
+            UID:abc001
+            DTSTART:20260223T080000Z
+            DTEND:20260223T094000Z
+            SUMMARY:Gyógypedagógiai szociológia ( - EC-I-2-1 csoport) - Dr. Czövek Andrea - Tanóra
+            LOCATION:ÉK.Als.3 (E.ÉK.Als.3)
+            END:VEVENT
+            BEGIN:VEVENT
+            UID:abc002
+            DTSTART:20260224T085500Z
+            DTEND:20260224T113000Z
+            SUMMARY:Tipikus és atipikus fejlődés pszichológiája ( - EC-I-2-1 csoport) - Dr. Dávid Mária - Tanóra
+            LOCATION:C.I.119 (E.C.I.119)
+            END:VEVENT
+            BEGIN:VEVENT
+            UID:abc003
+            DTSTART:20260225T060000Z
+            DTEND:20260225T074000Z
+            SUMMARY:Gyógypedagógiai fejlődéstan ( - EC-I-1) - Dr. Fodó Eszter - Tanóra
+            LOCATION:ÉK.Als.2 (E.ÉK.Als.2)
+            END:VEVENT
+            END:VCALENDAR
+        """.trimIndent()
+
+        val result = parser.parse(icsContent)
+
+        assertEquals(3, result.size)
+        assertEquals("Gyógypedagógiai szociológia", result[0].subject)
+        assertEquals("Tipikus és atipikus fejlődés pszichológiája", result[1].subject)
+        assertEquals("Gyógypedagógiai fejlődéstan", result[2].subject)
+    }
 }
