9cbd25890ee6f43ed9ee88dc3d15d9c81d2b36f9
garon <21.gubacsi.aron.mate@mechwart.com>
2026-04-16 14:29:48 +0200

    dispatcher rule added

---
+++ app/src/test/java/com/gubo/syllabusapp/core/util/MainDispatcherRule.kt
@@ -0,0 +1,25 @@
+package com.gubo.syllabusapp.core.util
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherRule(
+    val dispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description?) {
+        Dispatchers.setMain(dispatcher)
+        super.starting(description)
+    }
+
+    override fun finished(description: Description?) {
+        Dispatchers.resetMain()
+        super.finished(description)
+    }
+}
