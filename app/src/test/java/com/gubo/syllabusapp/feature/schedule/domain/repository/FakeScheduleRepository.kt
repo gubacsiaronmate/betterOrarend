ddd25ed3c921ff241efaab2f9d9ee23c0509c2f3
garon <21.gubacsi.aron.mate@mechwart.com>
2026-04-30 00:36:10 +0200

    bugfix

--- app/src/test/java/com/gubo/syllabusapp/feature/schedule/domain/repository/FakeScheduleRepository.kt
+++ app/src/test/java/com/gubo/syllabusapp/feature/schedule/domain/repository/FakeScheduleRepository.kt
@@ -8,9 +8,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import java.time.LocalDate
 
 class FakeScheduleRepository : ScheduleRepository {
-    private val semesters = MutableStateFlow<MutableList<Semester>>(mutableListOf())
-    private val sessions = MutableStateFlow<MutableList<ClassSession>>(mutableListOf())
-    private val userEvents = MutableStateFlow<MutableList<UserEvent>>(mutableListOf())
+    private val semesters = MutableStateFlow<List<Semester>>(emptyList())
+    private val sessions = MutableStateFlow<List<ClassSession>>(emptyList())
+    private val userEvents = MutableStateFlow<List<UserEvent>>(emptyList())
 
     override fun getSessionsForWeek(weekStart: LocalDate): Flow<List<ClassSession>> = sessions
 
@@ -24,12 +24,12 @@ class FakeScheduleRepository : ScheduleRepository {
     }
 
     override suspend fun addUserEvent(event: UserEvent) {
-        userEvents.value.add(event)
+        userEvents.value += event
     }
 
     override suspend fun switchActiveSemester(id: Long) {}
 
     override suspend fun deleteSemester(semester: Semester) {
-        semesters.value.remove(semester)
+        semesters.value = semesters.value.filter { it != semester }
     }
 }
